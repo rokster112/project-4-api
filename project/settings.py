cf590c252403a62dc872a302e25cb3d4c2f5a001
rokster112 <rokster112@gmail.com>
2023-01-23 19:18:35 +0000

    fixing CSRF link

--- project/settings.py
+++ project/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-sg@u0f3zzd@dcwdk*tipoydb9)xxz+-&gdzazoz+tw@r+r5%6&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['project-4-api-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['https://project-4-api-production.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['project-4-api-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://project-4-api-production.up.railway.app']
 
 
 # Application definition

a3b7aa25e254c282d5f09da52651e12abd4676e6
rokster112 <rokster112@gmail.com>
2023-01-24 00:25:07 +0000

    when deploying i get TemplateDoesNotExist error

--- project/settings.py
+++ project/settings.py
@@ -140,6 +140,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'client', "build", "static"),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

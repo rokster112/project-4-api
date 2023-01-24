ec154e3d60a5dcb89c32a30cf51d0472f95e1131
rokster112 <rokster112@gmail.com>
2023-01-24 01:30:19 +0000

    when deploying i get TemplateDoesNotExist error

--- project/settings.py
+++ project/settings.py
@@ -135,15 +135,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ROOT_URLCONF = 'project.urls'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'client', "build", "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'client', "build", "static"),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

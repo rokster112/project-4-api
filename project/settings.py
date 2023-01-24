5ca40173d0cdcdd6e953f716d482b99b36a83243
rokster112 <rokster112@gmail.com>
2023-01-24 17:08:59 +0000

    when deploying i get TemplateDoesNotExist error

--- project/settings.py
+++ project/settings.py
@@ -65,7 +65,8 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'client')
+      ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

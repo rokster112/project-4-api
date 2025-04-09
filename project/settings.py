801043498bc7fcd6077f5e9d7b41d4b05eef8059
rokas <rokster112@gmail.com>
2025-04-09 04:52:58 +0300

    editing template setting

--- project/settings.py
+++ project/settings.py
@@ -166,13 +166,17 @@ USE_TZ = True
 
 ROOT_URLCONF = 'project.urls'
 
-STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Serve React build files
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'client', 'build')]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'client', 'build', 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'client/build/static')
-]
 
 
 # Default primary key field type

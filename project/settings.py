1c027c783c0d39b67df8da4033cc772a8a4dbe8d
rokas <rokster112@gmail.com>
2025-04-09 04:17:04 +0300

    editing template setting

--- project/settings.py
+++ project/settings.py
@@ -174,7 +174,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'client/build/static')
 ]
 
-TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'client/build')]
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates/index.html')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
